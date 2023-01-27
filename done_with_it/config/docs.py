ee32f2c50ae1a2d199f592f05bbd86494f719245
karani-gk <geoffrey.kamundi@gmail.com>
2023-01-27 12:35:58 +0300

    feat: Initialize App

---
+++ done_with_it/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/done_with_it"
+# docs_base_url = "https://[org_name].github.io/done_with_it"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Done With It"
